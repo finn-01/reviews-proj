ccdb6b2f4a8a1713d4015f5d5a5bc4165029534a
finn-01 <nguyensam576@gmail.com>
2022-01-09 00:50:39 +0700

    setup

---
+++ src/Review.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
+
+import reviews from "./data";
+
+const Review = () => {
+	return <div>Review Component</div>;
+};
+
+export default Review;
